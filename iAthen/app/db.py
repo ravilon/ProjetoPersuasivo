549e527e94f8fea02c9c60a66d37758b1ad2ea23
Rávilon <ravilon.ads@inf.ufpel.edu.br>
2024-12-10 08:36:08 -0300

    Adding db connection JSON on .env

--- iAthen/app/db.py
+++ iAthen/app/db.py
@@ -1,4 +1,7 @@
 import firebase_admin
+import os
+import json
+from dotenv import load_dotenv
 from firebase_admin import credentials
 from firebase_admin import firestore
 
@@ -7,13 +10,14 @@ _db_client = None
 
 def get_db():
     global _db_client
+    load_dotenv()
+
     if _db_client is None:
-        # Path to your JSON key file
 
-        json_key_path = 'C:/Users/ravil/OneDrive/Documentos/GitHub/iAthenAPI/iAthen/db-connection/firebase_credentials.json'
-
+        json_key_path = json.loads(os.getenv('FIRESTORE'))
         # Initialize Firebase only once
         cred = credentials.Certificate(json_key_path)
+
         firebase_admin.initialize_app(cred)
 
         # Create Firestore client
